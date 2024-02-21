77f5f91d67145dc302bf0a45bb108d73f82226aa
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2024-02-21 12:15:25 +0300

    Moi pervyi kommit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://6ed38611-531a-4b51-bc77-0c8569bd1ecf.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
